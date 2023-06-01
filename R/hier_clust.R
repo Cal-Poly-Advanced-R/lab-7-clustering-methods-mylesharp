cff2c1266669e1884967ab8ff93204bae3392a40
ashkan-taghavi <125340010+ashkan-taghavi@users.noreply.github.com>
2023-06-01 12:53:35 -0700

    hier_clust update

--- R/hier_clust.R
+++ R/hier_clust.R
@@ -1,11 +1,37 @@
-# Hierarchical clustering function
+# Hierarchical clustering function without using hclust
 hier_clust <- function(data) {
+    n <- nrow(data)
     dist_matrix <- dist(data)  # Compute the distance matrix
 
-    hclust_result <- hclust(dist_matrix)  # Perform hierarchical clustering
+    # Initialize cluster assignments
+    clusters <- 1:n
 
-    cut_height <- 2 * median(hclust_result$height)
-    clusters <- cutree(hclust_result, h = cut_height)
+    # Main loop for hierarchical clustering
+    while (n > 1) {
+        # Find the minimum distance between clusters
+        min_dist <- Inf
+        merge_idx <- c(0, 0)
+        for (i in 1:(n-1)) {
+            for (j in (i+1):n) {
+                if (dist_matrix[i, j] < min_dist) {
+                    min_dist <- dist_matrix[i, j]
+                    merge_idx <- c(i, j)
+                }
+            }
+        }
+
+        # Merge the clusters with the minimum distance
+        clusters[clusters == merge_idx[2]] <- merge_idx[1]
+
+        # Update the distance matrix by merging the clusters
+        dist_matrix <- dist_matrix[-merge_idx[2], -merge_idx[2]]
+        dist_matrix <- rbind(dist_matrix, apply(dist_matrix, 2, min))
+        dist_matrix <- cbind(dist_matrix, apply(dist_matrix, 1, min))
+
+        # Remove the merged cluster
+        clusters <- clusters - (clusters > merge_idx[2])
+        n <- n - 1
+    }
 
     return(clusters)
 }
